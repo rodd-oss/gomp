8b6cadff38f67752f98e17be55bd7b4e410e05a0
milanjrodd <milanrodd@gmail.com>
2024-09-24 07:40:10 +0300

    fmt js

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-{ const sockets = new Set; Array(10).fill(1).forEach(() => { sockets.add(new WebSocket('https://gomptest.d.roddtech.ru/ws')) }) }
+{
+  const sockets = new Set();
+  Array(10)
+    .fill(1)
+    .forEach(() => {
+      sockets.add(new WebSocket("https://gomptest.d.roddtech.ru/ws"));
+    });
+}
