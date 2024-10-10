e6eeb75b45c68fb3b41dda19129137be385ad5e0
milanjrodd <milanrodd@gmail.com>
2024-10-10 16:33:12 +0300

    fmt

--- cmd/web-client-ws/main.go
+++ cmd/web-client-ws/main.go
@@ -15,8 +15,8 @@ func main() {
 	url = "ws" + url[4:]
 
 	inputs := client.NewInputs(input.AnyDevice)
-	transport := client.NewWsTransport(context.Background(), url)
 	config := client.NewConfig(640, 480, "MMO 360 no scope", e.WindowResizingModeEnabled)
+	transport := client.NewWsTransport(context.Background(), url)
 
 	gameClient := game.NewClient(inputs, transport, config)
 
