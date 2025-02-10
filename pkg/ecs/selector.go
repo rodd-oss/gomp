3043be0fcabcaeea7654382c0afe2b5cf4e0df37
AleD <adworld.aled@gmail.com>
2025-02-10 09:44:27 +0900

    Add selector without much reflection

--- pkg/ecs/selector.go
+++ pkg/ecs/selector.go
@@ -1,6 +1,7 @@
 package ecs
 
 import (
+	"fmt"
 	"iter"
 	"reflect"
 
@@ -157,6 +158,369 @@ func (s *Selector[T]) All() iter.Seq[T] {
 			var bundle T
 			s.pullComponentInstances(entId, &bundle)
 			if !yield(bundle) {
+				break
+			}
+		}
+	}
+}
+
+func pullComponentManager[T any](world *World, dst **ComponentManager[T]) error {
+	tTyp := reflect.TypeFor[T]()
+
+	for _, mng := range world.components {
+		if mng.getComponentType() == tTyp {
+			*dst = mng.(*ComponentManager[T])
+			return nil
+		}
+	}
+
+	return fmt.Errorf("world doesn't contains component %s", tTyp.String())
+}
+
+type Selector2[T1, T2 any] struct {
+	selectorBase
+	t1Manager *ComponentManager[T1]
+	t2Manager *ComponentManager[T2]
+}
+
+func (s *Selector2[T1, T2]) initInWorld(world *World) {
+	pullComponentManager(world, &s.t1Manager)
+	pullComponentManager(world, &s.t2Manager)
+	s.makeMasks([]AnyComponentManagerPtr{
+		s.t1Manager,
+		s.t2Manager,
+	},
+		[]AnyComponentManagerPtr{},
+	)
+}
+
+type Selector2Elements[T1 any, T2 any] struct {
+	C1 *T1
+	C2 *T2
+}
+
+func (s *Selector2[T1, T2]) All() iter.Seq[Selector2Elements[T1, T2]] {
+	return func(yield func(Selector2Elements[T1, T2]) bool) {
+		for entId := range s.matchedEnts.Keys() {
+			var elems Selector2Elements[T1, T2]
+			elems.C1 = s.t1Manager.Get(entId)
+			elems.C2 = s.t2Manager.Get(entId)
+			if !yield(elems) {
+				break
+			}
+		}
+	}
+}
+
+type Selector3[T1, T2, T3 any] struct {
+	selectorBase
+	t1Manager *ComponentManager[T1]
+	t2Manager *ComponentManager[T2]
+	t3Manager *ComponentManager[T3]
+}
+
+func (s *Selector3[T1, T2, T3]) initInWorld(world *World) {
+	pullComponentManager(world, &s.t1Manager)
+	pullComponentManager(world, &s.t2Manager)
+	pullComponentManager(world, &s.t3Manager)
+	s.makeMasks([]AnyComponentManagerPtr{
+		s.t1Manager,
+		s.t2Manager,
+		s.t3Manager,
+	},
+		[]AnyComponentManagerPtr{},
+	)
+}
+
+type Selector3Elements[T1, T2, T3 any] struct {
+	C1 *T1
+	C2 *T2
+	C3 *T3
+}
+
+func (s *Selector3[T1, T2, T3]) All() iter.Seq[Selector3Elements[T1, T2, T3]] {
+	return func(yield func(Selector3Elements[T1, T2, T3]) bool) {
+		for entId := range s.matchedEnts.Keys() {
+			var elems Selector3Elements[T1, T2, T3]
+			elems.C1 = s.t1Manager.Get(entId)
+			elems.C2 = s.t2Manager.Get(entId)
+			elems.C3 = s.t3Manager.Get(entId)
+			if !yield(elems) {
+				break
+			}
+		}
+	}
+}
+
+type Selector4[T1, T2, T3, T4 any] struct {
+	selectorBase
+	t1Manager *ComponentManager[T1]
+	t2Manager *ComponentManager[T2]
+	t3Manager *ComponentManager[T3]
+	t4Manager *ComponentManager[T4]
+}
+
+func (s *Selector4[T1, T2, T3, T4]) initInWorld(world *World) {
+	pullComponentManager(world, &s.t1Manager)
+	pullComponentManager(world, &s.t2Manager)
+	pullComponentManager(world, &s.t3Manager)
+	pullComponentManager(world, &s.t4Manager)
+	s.makeMasks([]AnyComponentManagerPtr{
+		s.t1Manager,
+		s.t2Manager,
+		s.t3Manager,
+		s.t4Manager,
+	},
+		[]AnyComponentManagerPtr{},
+	)
+}
+
+type Selector4Elements[T1, T2, T3, T4 any] struct {
+	C1 *T1
+	C2 *T2
+	C3 *T3
+	C4 *T4
+}
+
+func (s *Selector4[T1, T2, T3, T4]) All() iter.Seq[Selector4Elements[T1, T2, T3, T4]] {
+	return func(yield func(Selector4Elements[T1, T2, T3, T4]) bool) {
+		for entId := range s.matchedEnts.Keys() {
+			var elems Selector4Elements[T1, T2, T3, T4]
+			elems.C1 = s.t1Manager.Get(entId)
+			elems.C2 = s.t2Manager.Get(entId)
+			elems.C3 = s.t3Manager.Get(entId)
+			elems.C4 = s.t4Manager.Get(entId)
+			if !yield(elems) {
+				break
+			}
+		}
+	}
+}
+
+type Selector5[T1, T2, T3, T4, T5 any] struct {
+	selectorBase
+	t1Manager *ComponentManager[T1]
+	t2Manager *ComponentManager[T2]
+	t3Manager *ComponentManager[T3]
+	t4Manager *ComponentManager[T4]
+	t5Manager *ComponentManager[T5]
+}
+
+func (s *Selector5[T1, T2, T3, T4, T5]) initInWorld(world *World) {
+	pullComponentManager(world, &s.t1Manager)
+	pullComponentManager(world, &s.t2Manager)
+	pullComponentManager(world, &s.t3Manager)
+	pullComponentManager(world, &s.t4Manager)
+	pullComponentManager(world, &s.t5Manager)
+	s.makeMasks([]AnyComponentManagerPtr{
+		s.t1Manager,
+		s.t2Manager,
+		s.t3Manager,
+		s.t4Manager,
+		s.t5Manager,
+	},
+		[]AnyComponentManagerPtr{},
+	)
+}
+
+type Selector5Elements[T1, T2, T3, T4, T5 any] struct {
+	C1 *T1
+	C2 *T2
+	C3 *T3
+	C4 *T4
+	C5 *T5
+}
+
+func (s *Selector5[T1, T2, T3, T4, T5]) All() iter.Seq[Selector5Elements[T1, T2, T3, T4, T5]] {
+	return func(yield func(Selector5Elements[T1, T2, T3, T4, T5]) bool) {
+		for entId := range s.matchedEnts.Keys() {
+			var elems Selector5Elements[T1, T2, T3, T4, T5]
+			elems.C1 = s.t1Manager.Get(entId)
+			elems.C2 = s.t2Manager.Get(entId)
+			elems.C3 = s.t3Manager.Get(entId)
+			elems.C4 = s.t4Manager.Get(entId)
+			elems.C5 = s.t5Manager.Get(entId)
+			if !yield(elems) {
+				break
+			}
+		}
+	}
+}
+
+type Selector6[T1, T2, T3, T4, T5, T6 any] struct {
+	selectorBase
+	t1Manager *ComponentManager[T1]
+	t2Manager *ComponentManager[T2]
+	t3Manager *ComponentManager[T3]
+	t4Manager *ComponentManager[T4]
+	t5Manager *ComponentManager[T5]
+	t6Manager *ComponentManager[T6]
+}
+
+func (s *Selector6[T1, T2, T3, T4, T5, T6]) initInWorld(world *World) {
+	pullComponentManager(world, &s.t1Manager)
+	pullComponentManager(world, &s.t2Manager)
+	pullComponentManager(world, &s.t3Manager)
+	pullComponentManager(world, &s.t4Manager)
+	pullComponentManager(world, &s.t5Manager)
+	pullComponentManager(world, &s.t6Manager)
+	s.makeMasks([]AnyComponentManagerPtr{
+		s.t1Manager,
+		s.t2Manager,
+		s.t3Manager,
+		s.t4Manager,
+		s.t5Manager,
+		s.t6Manager,
+	},
+		[]AnyComponentManagerPtr{},
+	)
+}
+
+type Selector6Elements[T1, T2, T3, T4, T5, T6 any] struct {
+	C1 *T1
+	C2 *T2
+	C3 *T3
+	C4 *T4
+	C5 *T5
+	C6 *T6
+}
+
+func (s *Selector6[T1, T2, T3, T4, T5, T6]) All() iter.Seq[Selector6Elements[T1, T2, T3, T4, T5, T6]] {
+	return func(yield func(Selector6Elements[T1, T2, T3, T4, T5, T6]) bool) {
+		for entId := range s.matchedEnts.Keys() {
+			var elems Selector6Elements[T1, T2, T3, T4, T5, T6]
+			elems.C1 = s.t1Manager.Get(entId)
+			elems.C2 = s.t2Manager.Get(entId)
+			elems.C3 = s.t3Manager.Get(entId)
+			elems.C4 = s.t4Manager.Get(entId)
+			elems.C5 = s.t5Manager.Get(entId)
+			elems.C6 = s.t6Manager.Get(entId)
+			if !yield(elems) {
+				break
+			}
+		}
+	}
+}
+
+type Selector7[T1, T2, T3, T4, T5, T6, T7 any] struct {
+	selectorBase
+	t1Manager *ComponentManager[T1]
+	t2Manager *ComponentManager[T2]
+	t3Manager *ComponentManager[T3]
+	t4Manager *ComponentManager[T4]
+	t5Manager *ComponentManager[T5]
+	t6Manager *ComponentManager[T6]
+	t7Manager *ComponentManager[T7]
+}
+
+func (s *Selector7[T1, T2, T3, T4, T5, T6, T7]) initInWorld(world *World) {
+	pullComponentManager(world, &s.t1Manager)
+	pullComponentManager(world, &s.t2Manager)
+	pullComponentManager(world, &s.t3Manager)
+	pullComponentManager(world, &s.t4Manager)
+	pullComponentManager(world, &s.t5Manager)
+	pullComponentManager(world, &s.t6Manager)
+	pullComponentManager(world, &s.t7Manager)
+	s.makeMasks([]AnyComponentManagerPtr{
+		s.t1Manager,
+		s.t2Manager,
+		s.t3Manager,
+		s.t4Manager,
+		s.t5Manager,
+		s.t6Manager,
+		s.t7Manager,
+	},
+		[]AnyComponentManagerPtr{},
+	)
+}
+
+type Selector7Elements[T1, T2, T3, T4, T5, T6, T7 any] struct {
+	C1 *T1
+	C2 *T2
+	C3 *T3
+	C4 *T4
+	C5 *T5
+	C6 *T6
+	C7 *T7
+}
+
+func (s *Selector7[T1, T2, T3, T4, T5, T6, T7]) All() iter.Seq[Selector7Elements[T1, T2, T3, T4, T5, T6, T7]] {
+	return func(yield func(Selector7Elements[T1, T2, T3, T4, T5, T6, T7]) bool) {
+		for entId := range s.matchedEnts.Keys() {
+			var elems Selector7Elements[T1, T2, T3, T4, T5, T6, T7]
+			elems.C1 = s.t1Manager.Get(entId)
+			elems.C2 = s.t2Manager.Get(entId)
+			elems.C3 = s.t3Manager.Get(entId)
+			elems.C4 = s.t4Manager.Get(entId)
+			elems.C5 = s.t5Manager.Get(entId)
+			elems.C6 = s.t6Manager.Get(entId)
+			elems.C7 = s.t7Manager.Get(entId)
+			if !yield(elems) {
+				break
+			}
+		}
+	}
+}
+
+type Selector8[T1, T2, T3, T4, T5, T6, T7, T8 any] struct {
+	selectorBase
+	t1Manager *ComponentManager[T1]
+	t2Manager *ComponentManager[T2]
+	t3Manager *ComponentManager[T3]
+	t4Manager *ComponentManager[T4]
+	t5Manager *ComponentManager[T5]
+	t6Manager *ComponentManager[T6]
+	t7Manager *ComponentManager[T7]
+	t8Manager *ComponentManager[T8]
+}
+
+func (s *Selector8[T1, T2, T3, T4, T5, T6, T7, T8]) initInWorld(world *World) {
+	pullComponentManager(world, &s.t1Manager)
+	pullComponentManager(world, &s.t2Manager)
+	pullComponentManager(world, &s.t3Manager)
+	pullComponentManager(world, &s.t4Manager)
+	pullComponentManager(world, &s.t5Manager)
+	pullComponentManager(world, &s.t6Manager)
+	pullComponentManager(world, &s.t7Manager)
+	pullComponentManager(world, &s.t8Manager)
+	s.makeMasks([]AnyComponentManagerPtr{
+		s.t1Manager,
+		s.t2Manager,
+		s.t3Manager,
+		s.t4Manager,
+		s.t5Manager,
+		s.t6Manager,
+		s.t7Manager,
+		s.t8Manager,
+	},
+		[]AnyComponentManagerPtr{},
+	)
+}
+
+type Selector8Elements[T1, T2, T3, T4, T5, T6, T7, T8 any] struct {
+	C1 *T1
+	C2 *T2
+	C3 *T3
+	C4 *T4
+	C5 *T5
+	C6 *T6
+	C7 *T7
+	C8 *T8
+}
+
+func (s *Selector8[T1, T2, T3, T4, T5, T6, T7, T8]) All() iter.Seq[Selector8Elements[T1, T2, T3, T4, T5, T6, T7, T8]] {
+	return func(yield func(Selector8Elements[T1, T2, T3, T4, T5, T6, T7, T8]) bool) {
+		for entId := range s.matchedEnts.Keys() {
+			var elems Selector8Elements[T1, T2, T3, T4, T5, T6, T7, T8]
+			elems.C1 = s.t1Manager.Get(entId)
+			elems.C2 = s.t2Manager.Get(entId)
+			elems.C3 = s.t3Manager.Get(entId)
+			elems.C4 = s.t4Manager.Get(entId)
+			elems.C5 = s.t5Manager.Get(entId)
+			elems.C6 = s.t6Manager.Get(entId)
+			elems.C7 = s.t7Manager.Get(entId)
+			elems.C8 = s.t8Manager.Get(entId)
+			if !yield(elems) {
 				break
 			}
 		}
